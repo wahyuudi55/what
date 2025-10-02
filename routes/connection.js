41cf4eba6ac435e258a130c29368775fb94d7388
whyuxD-x <156475713+wahyuudi55@users.noreply.github.com>
2025-10-02 13:42:28 +0800

    Add files via upload

--- routes/connection.js
+++ routes/connection.js
@@ -154,7 +154,7 @@ const { imageMessage } = await generateWAMessageContent({ image: { url },}, {
     const text = body.slice(usedPrefix.length + command.length + 1).trim();
 
 const frpay = {key : { remoteJid:"0@s.whatsapp.net",id:"REQPAY"+Math.floor(Math.random()*1e6),participant:"0@s.whatsapp.net"},message:{requestPaymentMessage:{currencyCodeIso4217:"IDR",amount1000:String(Math.floor(Math.random()*5e5+1e5)),requestFrom:msg.sender,noteMessage:{stickerMessage:{url:"https://mmg.whatsapp.net/v/t62.15575-24/519772994_1880450272736126_8894645943848884651_n.enc?ccb=11-4&oh=01_Q5Aa2AGONA8PtRh9eqxl0EDqvxr_gJXfmHtX6uety7GfTwYnvg&oe=68986DC0&_nc_sid=5e03e0&mms3=true",fileSha256:"e9fy2V2mLQdwJfLlVkY+seoBfAafbvgKJ6K9bdw9jxM=",fileEncSha256:"o7iQKQWiOOBpWSYGY6oefYggCmDU7UO9W1tZx8J6aoY=",mediaKey:"o2mu3wa+ynfKJ1kjQDMtDD5RBSBH6tTR801EFz9coDQ=",mimetype:"image/webp",height:64,width:64,directPath:"/o1/v/t24/f2/m238/AQNTXHve8NJAcYVaqLBmTCMx1r-hxBGF-ht85xYI1NlmMAW40ZSd5NJAAxzEedNN7xguj6KugTE4EOOYdh1bwzguSPLM7DRknWk3YydxiQ?ccb=9-4&oh=01_Q5Aa2AFmBMIBTxdhwGiiXKQgFIwREBJW1kHuqDutw2XEOzhfNg&oe=68984648&_nc_sid=e6ed6c"}},amount:{value:"2420",offset:1e3,currencyCode:"IDR"},background:{id:"100",fileLength:"928283",width:1e3,height:1e3,mimetype:"image/webp",placeholderArgb:4294901760,textArgb:4294967295,subtextArgb:4278190080}}}};
-const forder = {key : {remoteJid:"status@broadcast",fromMe:false,id:"BAE5C9E3C9A6C8D6",participant:"0@s.whatsapp.net"},message:{orderMessage:{productId:"8569472943180260",title:null,description:null,currencyCode:"IDR",priceAmount1000:"91000",message:text,thumbnailUrl:"https://img100.pixhost.to/images/718/540724665_a7bf39d4.jpg",surface:"whyuxD"}}};
+const forder = {key : {remoteJid:"status@broadcast",fromMe:false,id:"BAE5C9E3C9A6C8D6",participant:"0@s.whatsapp.net"},message:{orderMessage:{productId:"8569472943180260",title:null,description:null,currencyCode:"IDR",priceAmount1000:"91000",message:"whyuxD",thumbnailUrl:"https://img100.pixhost.to/images/718/540724665_a7bf39d4.jpg",surface:"whyuxD"}}};
 const fpay = { key: { fromMe: false, participant: "0@s.whatsapp.net", ...(msg.chat ? { remoteJid: "status@broadcast" } : {}) }, message: { "paymentInviteMessage": { "serviceType": 3, "expiryTimestamp": "200"}}};
 
     const reply = async (teks) => {
@@ -212,7 +212,7 @@ let mg = generateWAMessageFromContent(
                       url: "",
                       productImageCount: 1,
                     },
-                    businessOwnerJid: "6283189053897@s.whatsapp.net",
+                    businessOwnerJid: "6287864807845@s.whatsapp.net",
                   },
                   hasMediaAttachment: false
                 }),
@@ -235,7 +235,7 @@ let mg = generateWAMessageFromContent(
       },
     },
   },
-  { userJid: msg.sender, quoted : forder }
+  { userJid: msg.sender, quoted : fpay }
 );
 await sock.relayMessage(mg.key.remoteJid, mg.message, {
   messageId: mg.key.id,
